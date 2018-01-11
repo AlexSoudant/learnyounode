7a7c644c4f4a666cb3ba3c6a1b6ce561f5e7c498
Alex Soudant <soudant.alex@gmail.com>
2018-01-11 17:56:16 +0100

    ok

--- program.js
+++ program.js
@@ -1,38 +1,15 @@
-const http = require('http')
-const BufferList = require('bl')
+const net = require('net')
+const strftime = require('strftime')
 
-let URL_to_collect = process.argv[2] 
-let bl = new BufferList()
-let phrase = ""
+const port = process.argv[2]
 
-http.get(URL_to_collect, function (response) {
-  response.setEncoding('utf8').on('data', function (data) {
-    // console.log("data", data)
-    bl.append(data)
-  })
-
-  // response.pipe(bl(function (err, data) { 
-  //   data.toString()
-  // }))
-
-  response.on('error', function (err) {
-    console.log(err)
-  })
-  response.on("end", function(err){
-    // console.log(bl._bufs.map(buffer => buffer.toString()))
-    list_characters = bl._bufs.map(buffer => buffer.toString())
-
-    nb_characters = bl._bufs.reduce((acc, element) => {
-      return acc + element.toString().length
-    }, 0)
-
-    console.log(nb_characters)
-    phrase = bl._bufs.map(buffer => phrase.concat(buffer.toString()))
-    console.log(list_characters.join(''))
-  })
+var server = net.createServer(function (socket) {
+  // socket handling logic
+  socket.end(strftime( "%F %R", new Date())+"\n" )
 })
 
+server.listen(port)
 
-  
+
 
 
