7bfd5b1d493e04fbadc215a867864dcb1f85b8b6
Alex Soudant <soudant.alex@gmail.com>
2018-01-11 16:58:54 +0100

    evolution crade

--- program.js
+++ program.js
@@ -5,11 +5,24 @@ let URL_1 = process.argv[2]
 let URL_2= process.argv[3] 
 let URL_3 = process.argv[4] 
 
+results = []
+
 let URL_list = [URL_1, URL_2, URL_3]
 
 URL_list.map(URL_called => {
+
+
+  results.push(get_http_response(URL_called))
+
+})
+
+console.log(results)
+results.map(result => console.log(result))
+
+function get_http_response(URL_called){
   let bl = new BufferList()
   let phrase = ""
+  let list_characters = []
 
   http.get(URL_called, function (response) {
     response.setEncoding('utf8').on('data', function (data) {
@@ -29,7 +42,7 @@ URL_list.map(URL_called => {
       // console.log(bl._bufs.map(buffer => buffer.toString()))
 
       // console.log(bl)
-      list_characters = bl._bufs.map(buffer => buffer.toString())
+      this.list_characters = bl._bufs.map(buffer => buffer.toString())
       // console.log(list_characters)
 
       nb_characters = bl._bufs.reduce((acc, element) => {
@@ -38,11 +51,13 @@ URL_list.map(URL_called => {
   
       // console.log(nb_characters)
       phrase = bl._bufs.map(buffer => phrase.concat(buffer.toString()))
-      console.log(list_characters.join(''))
+      // return list_characters.join('')
     })
   })
 
-})
+  return list_characters.join('')
+
+} 
 
 
 
