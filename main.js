46c4fef985464b52cbc269536e5a53e09c8e6829
Alex Soudant <soudant.alex@gmail.com>
2018-01-11 15:21:14 +0100

    soluce

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let file_extention = process.argv[3]
 let result = undefined
 
 module(directory_name, file_extention, (error, result) => {
+  if (error){
+    console.log(error)
+  }
   result.map(filtered_elem => console.log(filtered_elem))
 })
 // module.component(directory_name, file_extention, result)
--- program.js
+++ program.js
@@ -3,27 +3,18 @@
 var fs = require('fs')
 
 
-// function filter_files(){
-//   filtered_result = result.filter( file => 
-//     file.split('.')[1] == file_extention
-//   )
-
-//   return filtered_result
-
-// }
-
 module.exports = function promise_read_dir(path_to_file, file_extention, callback){
   
-  fs.readdir(path_to_file, ( (error, list) => {
+  fs.readdir(path_to_file,  (error, list) => {
 
     if (error){
-      console.log(error)
+      return callback(error)
     }
     let filtered_result = list.filter( file => 
       file.split('.')[1] == file_extention
     )
     return callback(null, filtered_result)
-  }))
+  })
 }
 
   
