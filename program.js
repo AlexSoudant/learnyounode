26b0672e03a714f3ab74a132ef83bc1314d50759
Alex Soudant <soudant.alex@gmail.com>
2018-01-11 11:31:58 +0100

    soluce for sync

--- program.js
+++ program.js
@@ -5,15 +5,20 @@ var fs = require('fs')
 let path_to_file = process.argv[2]
 let result = undefined
 
-function log_file_content(){
-  console.log(result)
-}
+// function log_file_content(){
+//   console.log(result)
+// }
 
-function read_file(path_to_file, callback){
-  fs.readFile(path_to_file, 'utf8', function reading_done(error, file_content) {
-    result = file_content.split(/\r\n|\r|\n/).length - 1
-    callback()
-  })
-}
+// function read_file(path_to_file, callback){
+//   fs.readFile(path_to_file, 'utf8', function reading_done(error, file_content) {
+//     result = file_content.split(/\r\n|\r|\n/).length - 1
+//     callback()
+//   })
+// }
 
-read_file(path_to_file, log_file_content)
+// read_file(path_to_file, log_file_content)
+
+result = fs.readFileSync(path_to_file, 'utf8')
+result = result.split(/\r\n|\r|\n/).length - 1
+
+console.log(result)
