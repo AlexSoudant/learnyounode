bd69213bf646d2acbbacd540aa23bae6d7e56dc0
Alex Soudant <soudant.alex@gmail.com>
2018-01-11 13:55:13 +0100

    soluce filtered ls

--- program.js
+++ program.js
@@ -2,18 +2,29 @@
 
 var fs = require('fs')
 
-let path_to_file = process.argv[2]
+let directory_name = process.argv[2]
+let file_extention = process.argv[3]
 let result = undefined
 
-function log_file_content(){
-  console.log(result)
+
+function filter_files(){
+  //console.log(result)
+  filtered_result = result.filter( file => 
+    file.split('.')[1] == file_extention
+  )
+  // console.log(filtered_result[0])
+  // console.log(filtered_result[1])
+  // console.log(filtered_result[2])
+  filtered_result.map(filtered_elem => console.log(filtered_elem))
+
 }
 
-function read_file(path_to_file, callback){
-  fs.readFile(path_to_file, 'utf8', function reading_done(error, file_content) {
-    result = file_content.split(/\r\n|\r|\n/).length - 1
+function read_dir(path_to_file, callback){
+  
+  fs.readdir(path_to_file, ( (error, list) => {
+    result = list
     callback()
-  })
+  }))
 }
 
-read_file(path_to_file, log_file_content)
+read_dir(directory_name, filter_files)
