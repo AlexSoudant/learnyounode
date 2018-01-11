131af4c7b2821eba841b9bfd3a115564319c93bd
Alex Soudant <soudant.alex@gmail.com>
2018-01-11 11:19:57 +0100

    soluce to myfirstio by alex

--- program.js
+++ program.js
@@ -2,6 +2,18 @@
 
 var fs = require('fs')
 
-//fs.readFileSync('/path/to/file')
+let path_to_file = process.argv[2]
+let result = undefined
 
+function log_file_content(){
+  console.log(result)
+}
 
+function read_file(path_to_file, callback){
+  fs.readFile(path_to_file, 'utf8', function reading_done(error, file_content) {
+    result = file_content.split(/\r\n|\r|\n/).length - 1
+    callback()
+  })
+}
+
+read_file(path_to_file, log_file_content)
