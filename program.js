60af15406677b721ad3f10aaceb166e7b3c57c22
Alex Soudant <soudant.alex@gmail.com>
2018-01-11 16:27:23 +0100

    reducer is not functal

--- program.js
+++ program.js
@@ -1,15 +1,33 @@
 const http = require('http')
+const BufferList = require('bl')
 
-http.get(process.argv[2], function (response) {
+let URL_to_collect = process.argv[2] 
+let bl = new BufferList()
+let phrase = ""
+const reducer = (accumulator, currentValue) => accumulator + Number(currentValue.toString().length)
+
+http.get(URL_to_collect, function (response) {
   response.setEncoding('utf8').on('data', function (data) {
-    console.log(data)
+    // console.log("data", data)
+    bl.append(data)
   })
+
+  // response.pipe(bl(function (err, data) { 
+  //   data.toString()
+  // }))
 
   response.on('error', function (err) {
     console.log(err)
   })
   response.on("end", function(err){
+    // console.log(bl._bufs.map(buffer => buffer.toString()))
+    list_characters = bl._bufs.map(buffer => buffer.toString())
 
+    nb_characters = bl._bufs.reduce(reducer)
+
+    console.log(nb_characters)
+    phrase = bl._bufs.map(buffer => phrase.concat(buffer.toString()))
+    console.log(list_characters.join(' '))
   })
 })
 
