e71c932a57f2ad1dd856a5e6595cd487b090d22b
Alex Soudant <soudant.alex@gmail.com>
2018-01-12 09:43:56 +0100

    soluce

--- program.js
+++ program.js
@@ -1,17 +1,24 @@
 const http = require('http')
 const fs = require('fs')
+const map = require('through2-map')
 
 const port = process.argv[2]
-const pathFile = process.argv[3]
+// const pathFile = process.argv[3]
+
 
 var server = http.createServer(function (req, res) {
-  // socket handling logic
-  console.log('file', pathFile)
-  res.writeHead(200, {'content-type': 'text/plain'})
-  fs.createReadStream(pathFile).pipe(res)
+
+  if (req.method !== 'POST') {
+    return res.end('send me a POST\n')
+  }
+
+  req.pipe(map(function (chunk){
+    return chunk.toString().toUpperCase()
+  })).pipe(res)
+
 })
 
-server.listen(parseInt(port))
+server.listen(Number(port))
 
 
 
